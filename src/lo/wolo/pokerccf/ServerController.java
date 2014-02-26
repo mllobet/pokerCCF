bc3c9590dd1a165d43179d72ab25ea8990439a9c
Albert <albertmg7@gmail.com>
2014-02-26 06:15:56 +0100

    Added flip cards feature

--- src/lo/wolo/pokerccf/ServerController.java
+++ src/lo/wolo/pokerccf/ServerController.java
@@ -91,6 +91,19 @@ public class ServerController extends AbstractServiceUsingActivity implements On
 	TextView potView = null;
 	TextView curBetView = null;
 	
+	static final int cardDrawables[] = {
+		R.drawable.card_00, R.drawable.card_01, R.drawable.card_02, R.drawable.card_03, R.drawable.card_04,
+		R.drawable.card_05, R.drawable.card_06, R.drawable.card_07, R.drawable.card_08, R.drawable.card_09,
+		R.drawable.card_10, R.drawable.card_11, R.drawable.card_12, R.drawable.card_13, R.drawable.card_14,
+		R.drawable.card_15, R.drawable.card_16, R.drawable.card_17, R.drawable.card_18, R.drawable.card_19,
+		R.drawable.card_20, R.drawable.card_21, R.drawable.card_22, R.drawable.card_23, R.drawable.card_24,
+		R.drawable.card_25, R.drawable.card_26, R.drawable.card_27, R.drawable.card_28, R.drawable.card_29,
+		R.drawable.card_30, R.drawable.card_31, R.drawable.card_32, R.drawable.card_33, R.drawable.card_34,
+		R.drawable.card_35, R.drawable.card_36, R.drawable.card_37, R.drawable.card_38, R.drawable.card_39,
+		R.drawable.card_40, R.drawable.card_41, R.drawable.card_42, R.drawable.card_43, R.drawable.card_44,
+		R.drawable.card_45, R.drawable.card_46, R.drawable.card_47, R.drawable.card_48, R.drawable.card_49,
+		R.drawable.card_50, R.drawable.card_51
+	};
 
 	private ServerController myself;
 
@@ -177,6 +190,28 @@ public class ServerController extends AbstractServiceUsingActivity implements On
 	}
 	
 
+	public void setCard(int cardHash, int cardNum) {
+		switch(cardNum) {
+		
+		case 1:
+			card1.setImageResource(cardDrawables[cardHash]);
+			break;
+		case 2:
+			card2.setImageResource(cardDrawables[cardHash]);
+			break;
+		case 3:
+			card3.setImageResource(cardDrawables[cardHash]);
+			break;
+		case 4:
+			card4.setImageResource(cardDrawables[cardHash]);
+			break;
+		case 5:
+			card5.setImageResource(cardDrawables[cardHash]);
+			break;		
+		}
+	}
+	
+	
 	//This is a callback on button click.
 	@Override
 	public void onClick(View view) {
